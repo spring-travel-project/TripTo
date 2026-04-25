393239b525d7724ca3104e671d8364bcaad71a71
sebin9731 <sebin9731@gmail.com>
2026-04-26 08:26:51 +0900

    동행게시글 등록, 상세 기능  구현

--- src/main/java/com/tripto/dto/ReportDTO.java
+++ src/main/java/com/tripto/dto/ReportDTO.java
@@ -1,5 +1,23 @@
 package com.tripto.dto;
 
+import lombok.Data;
+
+@Data
 public class ReportDTO {
 
+    private int seqReport;
+
+    // 신고한 회원
+    private int seqMember;
+
+    // 신고 대상 타입: USER / BOARD / TRAVEL
+    private String targetType;
+
+    // 신고 대상 번호
+    private int seqTarget;
+
+    // 신고 사유
+    private String reason;
+
+    private String createDate;
 }
